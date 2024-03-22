76795c4676525b1dfd57bd7306b1984f0e100558
alwegr <alinagor2004@gamil.com>
2024-03-22 12:07:17 +0300

    переделанный sidebar

--- src/romashovsoft_document_flow/client/src/components/Navbar/Navbar.jsx
+++ src/romashovsoft_document_flow/client/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom'
 import style from './Navbar.css'
+import { IoDocumentsOutline } from "react-icons/io5";
+import { GoPeople } from "react-icons/go";
+import { IoMenu } from "react-icons/io5";
 
-export const Navbar = () => {
+
+
+export const Navbar = ({children}) => {
+  const [isOpen, setIsOpen] = useState(true);
+    function toggle() {
+        setIsOpen(!isOpen);
+    }
+
+  const menuItem=[
+    {
+      path: '/documents',
+      name: 'Документы',
+      icon: <IoDocumentsOutline/>
+    },
+    {
+      path: '/employee',
+      name: 'Сотрудники',
+      icon: <GoPeople/>
+    }
+  ]
   return (
-    <aside className={style.aside}>
-      <nav className={style.menu}>
-        <ul>
-          <li>
-            <NavLink to={'/'}>Главная</NavLink>
-          </li>
-          <li>
-            <NavLink to={'/documents'}>Документы</NavLink>
-          </li>
-          <li>
-            <NavLink to={'/employee'}>Сотрудники</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </aside>
+    <div className='container'>
+      <aside style={{ width: isOpen ? '300px' : '50px' }} className={style.sidebar}>
+        <div className={style.top_section}>
+          <Link to='/'>
+            <h1 style={{ display: isOpen ? 'block' : 'none' }} className={style.logo}>Logo</h1>
+          </Link>
+          <div style={{ marginLeft: isOpen ? '130px' : '0px' }} className={style.bars}>
+            <IoMenu onClick={toggle} />
+          </div>
+        </div>
+        <div>
+        {
+          menuItem.map((item, index) => (
+            <NavLink to={item.path} key={index} className={style.link} aria-activedescendant='active'>
+              <div className={style.icon}>{item.icon}</div>
+              <div style={{ display: isOpen ? 'block' : 'none' }} className={style.link_text}>{item.name}</div>
+            </NavLink>
+          ))
+        }
+        </div>
+      </aside>
+      <main>{children}</main>
+    </div>
+    
+    
   )
 }
--- src/romashovsoft_document_flow/client/src/pages/Main/MainPages.jsx
+++ src/romashovsoft_document_flow/client/src/pages/Main/MainPages.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import style from './Main.css'
 
 export const MainPages = () => {
   return (
-    <main className={style.main}>
+    
       <div>MainPages</div>
-    </main>
+  
   )
 }
